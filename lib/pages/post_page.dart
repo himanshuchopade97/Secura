8b062448ee68e3f3099d5d78038bdb3e4cf02ca8
himanshuchopade <himanshuchopade97@gmail.com>
2024-09-25 18:16:34 +0530

    added post page

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:secura/components/my_post_tile.dart';
+import 'package:secura/helper/navigate_pages.dart';
 import 'package:secura/models/post.dart';
 
 /*
@@ -23,7 +25,19 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.post.message),
+        // title: Text(widget.post.message),
+        foregroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: ListView(
+        children: [
+          //post
+          MyPostTile(
+            post: widget.post,
+            onUserTap: () => goUserPage(context, widget.post.uid),
+            onPostTap: () {},
+          )
+          //comments
+        ],
       ),
     );
   }
